76a5ce33bed87fcb7f41dce34c001fd025aeb59f
OlenaKyz <kyz102030@gmail.com>
2024-02-17 11:52:38 +0200

    1

--- CasinoProject/Program.cs
+++ CasinoProject/Program.cs
@@ -1,1 +1,46 @@
 ﻿
+
+
+
+
+
+
+
+using System.Numerics;
+
+abstract class Game
+{
+
+    public void AddPlayer() { }
+    public void GameOn() { }
+}
+
+class Game21 : Game
+{
+    List<Player> players = new List<Player>();
+
+    public void AddPlayer()
+    {
+
+    }
+
+    public void GameOn()
+    {
+
+    }
+
+}
+
+
+
+class Player
+{
+    public string Name { get; set; }
+    public int Balance { get; set; }
+    Player(string name, int balance)
+    {
+        Name = name;
+        Balance = balance;
+    }
+
+}
