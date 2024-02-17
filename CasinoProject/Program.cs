707f28b7e4be8f2c1aa8585825276293c6a97e41
OlenaKyz <kyz102030@gmail.com>
2024-02-17 13:35:59 +0200

    2

--- CasinoProject/Program.cs
+++ CasinoProject/Program.cs
@@ -1,12 +1,9 @@
-﻿
-
-
-
-
-
-
-
+﻿using System.Collections;
 using System.Numerics;
+
+Random rand= new Random();
+
+
 
 abstract class Game
 {
@@ -17,7 +14,7 @@ abstract class Game
 
 class Game21 : Game
 {
-    List<Player> players = new List<Player>();
+    List<ArrayList> players = new List<ArrayList>();
 
     public void AddPlayer()
     {
@@ -25,13 +22,29 @@ class Game21 : Game
         string name=Console.ReadLine();
         Console.WriteLine("deposit:");
         int deposit=Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine();
-        Console.WriteLine("");
+        Console.WriteLine("For registration you get 100 bonuses.");
+        ArrayList list = new ArrayList() { 0, new Player(name, deposit + 100) };
+        players.Add(list);
+    }
+
+    public int NumCard(int val)
+    {
+        if(val==11) return 2;
+        if(val==12) return 3;
+        if(val==13) return 4;
+        return val;
     }
 
     public void GameOn()
     {
+        int vin = 0;
+        while(vin!=1)
+        {
+            for(int  i=0; i<players.Count; i++)
+            {
 
+            }
+        }
     }
 
 }
@@ -42,7 +55,7 @@ class Player
 {
     public string Name { get; set; }
     public int Balance { get; set; }
-    Player(string name, int balance)
+    public Player(string name, int balance)
     {
         Name = name;
         Balance = balance;
